e0b5076ce02291023cff9649e28ef1981fc0613a
Pedro Ananias <pedro.ananias@unesp.br>
2020-07-01 11:49:30 -0300

    - Primeira versão

--- README.md
+++ README.md
@@ -1,3 +1,78 @@
-# Google Earth Engine Api Image Downloader
+### Algal Bloom Yearly Occurrence
 
-Google Earth Engine Api Image Downloader
+Module responsable for extracting Algal Bloom Yearly Occurences in a region of interest based on images from Google Earth Engine API
+
+
+
+### Dependencies
+
+- Python 3.7.7 64-bit ou superior
+- Modules: oauth2client earthengine-api matplotlib pandas numpy requests pillow natsort geojson argparse logging hashlib joblib gctraceback warnings
+
+
+
+### Attention, before running:
+
+Before running the script and after installing the libraries, you must authenticate with the Google Earth Engine API using the following command:
+
+```
+earthengine authenticate
+```
+
+
+
+#### How to execute the default script?
+
+python /path/to/abyo/script.py --lat_lon=-48.84725671390528,-22.04547298853004,-47.71712046185493,-23.21347463046867 --name=bbhr --date_start=1985-01-01 --date_end=1018-12-31
+
+
+
+
+### What are the results?
+
+The script will generate annual results of occurrence of algae and clouds blooming in the inserted study area. Therefore, a folder located in 'data' is created and named based on the date and version of the script executed. Example: /path/to/abyo/data/20200701_111426[v=V1-bbhr,date_start=1985-01-01,date_end=2018-12-31]
+
+The following results are generated:
+
+- timeseries.csv (Annual time series of pixels, latitude, longitude, sum of the Slope and labels and occurrences of algae bloom (based on the threshold of the Slope algorithm) and clouds)
+- occurrences.png (Graphs of occurrences separated annually from algal blooms)
+- occurrences_clouds.png (Graphs of occurrences separated annually from clouds)
+- ocurrences.json (GeoJSON of occurrences with parameters (occurrence, cloud and year) that can be imported into QGIS and filtered)
+
+
+
+### Exporting GeoTIFFs to Google Drive
+
+When using the 'save_collection_tiff' function, a folder called 'abyo.data.tiff' will be created in the Google Drive of the user who authenticated. Daily images used in the composition of the annual time series are saved. These images will be separated by the following bands: Red, Green, Blue and Label (-1 = cloud, 0 = regular and 1 = anomaly). However, after running the Abyo script, images are likely to take a while to be inserted into Drive due to processing time. It is necessary to wait approximately 1 day until they are all available, depending on the size of the study area.
+
+
+
+### Exemplo
+
+```
+# folder where to save results
+folder = "/path/to/desired/folder"
+
+# create algorithm object
+abyo = abyo.Abyo(lat_lon="-48.84725671390528,-22.04547298853004,-47.71712046185493,-23.21347463046867",
+                date_start=dt.strptime("1985-01-01", "%Y-%m-%d"),
+                date_end=dt.strptime("2018-12-31", "%Y-%m-%d"),
+                sensor="landsat578",
+                cache_path=folder, 
+                force_cache=False)
+
+# creating yearly timeseries
+abyo.process_timeseries_data()
+
+# save timeseries in csv file
+abyo.save_dataset(df=abyo.df_timeseries, path=folder+'/timeseries.csv')
+
+# save ocurrences plot (yearly and clouds)
+abyo.save_occurrences_plot(df=abyo.df_timeseries, folder=folder)
+
+# save geojson occurrences
+abyo.save_occurrences_geojson(df=abyo.df_timeseries, folder=folder)
+
+# save daily images to Google Drive
+abyo.save_collection_tiff()
+```
---
+++ script.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#########################################################################################################################################
+# ### ABYO - Algal Bloom Yearly Occurences
+# ### Script responsible for executing Algal Bloom Yearly Occurences in a region of interest
+#                
+# ### Change History
+# - Version 1: Repository creation
+#########################################################################################################################################
+
+# ### Version
+version = "V1"
+
+
+
+# ### Module imports
+
+# Main
+import ee
+import pandas as pd
+import math
+import requests
+import time
+import warnings
+import os
+import sys
+import argparse
+import logging
+import traceback
+
+# Sub
+from datetime import datetime as dt
+from datetime import timedelta
+
+# Extras modules
+from modules import misc, gee, abyo
+
+
+# ### Script args parsing
+
+# starting arg parser
+parser = argparse.ArgumentParser(description=version)
+
+# create arguments
+parser.add_argument('--lat_lon', dest='lat_lon', action='store', default="-48.84725671390528,-22.04547298853004,-47.71712046185493,-23.21347463046867",
+                   help="Two diagnal points (Latitude 1, Longitude 1, Latitude 2, Longitude 2) of the study area")
+parser.add_argument('--date_start', dest='date_start', action='store', default="1985-01-01",
+                   help="Date to start time series")
+parser.add_argument('--date_end', dest='date_end', action='store', default="2018-12-31",
+                   help="Date to end time series")
+parser.add_argument('--name', dest='name', action='store', default="bbhr",
+                   help="Place where to save generated files")
+
+# parsing arguments
+args = parser.parse_args()
+
+
+
+
+# ### Start
+
+try:
+
+  # Start script time counter
+  start_time = time.time()
+
+  # Google Earth Engine API initialization
+  ee.Initialize()
+
+
+
+  # ### Working directory
+
+  # Data path
+  folderRoot = os.path.dirname(os.path.realpath(__file__))+'/data'
+  if not os.path.exists(folderRoot):
+    os.mkdir(folderRoot)
+
+  # Images path
+  folderCache = os.path.dirname(os.path.realpath(__file__))+'/cache'
+  if not os.path.exists(folderCache):
+    os.mkdir(folderCache)
+
+
+  
+  # ### ABYO execution
+
+  # folder to save results from algorithm at
+  folder = folderRoot+'/'+dt.now().strftime("%Y%m%d_%H%M%S")+'[v='+str(version)+'-'+str(args.name)+',date_start='+str(args.date_start)+',date_end='+str(args.date_end)+']'
+  if not os.path.exists(folder):
+    os.mkdir(folder)
+
+  # create algorithm
+  abyo = abyo.Abyo(lat_lon=args.lat_lon,
+                   date_start=dt.strptime(args.date_start, "%Y-%m-%d"),
+                   date_end=dt.strptime(args.date_end, "%Y-%m-%d"),
+                   sensor="landsat578",
+                   cache_path=folderCache, 
+                   force_cache=False)
+
+  # preprocessing
+  abyo.process_timeseries_data()
+
+  # save timeseries in csv file
+  abyo.save_dataset(df=abyo.df_timeseries, path=folder+'/timeseries.csv')
+
+  # create plot
+  abyo.save_occurrences_plot(df=abyo.df_timeseries, folder=folder)
+
+  # save geojson occurrences and clouds
+  abyo.save_occurrences_geojson(df=abyo.df_timeseries, folder=folder)
+
+  # save images to Google Drive
+  abyo.save_collection_tiff()
+
+  # ### Script termination notice
+  script_time_all = time.time() - start_time
+  debug = "***** Script execution completed successfully (-- %s seconds --) *****" %(script_time_all)
+  print()
+  print(debug)
+
+except:
+
+    # ### Script execution error warning
+
+    # Execution
+    print()
+    print()
+    debug = "***** Error on script execution: "+str(traceback.format_exc())
+    print(debug)
+
+    # Removes the folder created initially with the result of execution
+    script_time_all = time.time() - start_time
+    debug = "***** Script execution could not be completed (-- %s seconds --) *****" %(script_time_all)
+    print(debug)
